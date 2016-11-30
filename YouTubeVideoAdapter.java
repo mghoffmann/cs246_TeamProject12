70288c59565f8747a98e0f17372b7504cd9f4cdb
Michael Hoffmann <mghoffmann@gmail.com>
2016-11-30 09:35:53 -0700

    Added all the java files

---
+++ YouTubeVideoAdapter.java
@@ -0,0 +1,22 @@
+package socialmedia;
+
+/**
+ * Adapter class for exposing data from YouTubeVideo class via
+ * the SocialMediaEntry interface.
+ */
+class YouTubeVideoAdapter implements SocialMediaEntry {
+
+   YouTubeVideo adaptee;
+
+   public YouTubeVideoAdapter(YouTubeVideo adaptee) {
+      this.adaptee = adaptee;
+   }
+
+   public String getPostText() {
+      return adaptee.getTitle() + "\n" + adaptee.getDescription();
+   }
+
+   public String getUser() {
+      return adaptee.getAuthor(); 
+   }
+}
